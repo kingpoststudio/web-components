922c74a180d1476f55f808e561bf6765da469ede
tlhfckoctbcr <stu@campgrantnc.com>
2022-07-21 22:10:27 -0400

    Button color variants, add main.css

--- src/components/Button/Button.stories.ts
+++ src/components/Button/Button.stories.ts
@@ -1,11 +1,18 @@
 import { html } from 'lit';
 import './Button';
+import '../../styles/main.css';
 
 export default {
   title: 'Button',
   component: 'kps-button',
 };
 
-export const PrimaryButton = () => html`
-  <kps-button primary>Primary</kps-button>
+export const Colors = () => html`
+  <div>
+    <kps-button color="primary">Primary</kps-button>
+    <kps-button color="secondary">Secondary</kps-button>
+    <kps-button color="success">Success</kps-button>
+    <kps-button color="warning">Warning</kps-button>
+    <kps-button color="error">Error</kps-button>
+  </div>
 `;
