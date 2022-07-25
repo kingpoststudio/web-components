041ae03cb7e7696e6d59cfa83935e9ac0f2ca8f0
tlhfckoctbcr <stu@campgrantnc.com>
2022-07-25 19:43:54 -0400

    Nav CTA w/ Button integration

--- src/components/Button/Button.ts
+++ src/components/Button/Button.ts
@@ -81,6 +81,13 @@ export default class Button extends LitElement {
     }
   `;
 
+  /**
+   * onSlotChange
+   * @param e
+   * @returns void
+   * Listening for a slotchange event to detect when the HubSpot CTA HTML is added to the slot.
+   * Once the CTA is available, we take it's href and target attributes and set them on the <a> tag.
+   */
   onSlotChange(e: { target: HTMLSlotElement }) {
     if (!this.isCta) return;
     const ctaLink = e.target.assignedElements()[0].querySelector('a');
