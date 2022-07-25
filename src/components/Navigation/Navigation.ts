2193c18b85378027f6e88f9ce433e5e93b0e0a63
tlhfckoctbcr <stu@campgrantnc.com>
2022-07-25 13:54:51 -0400

    Navigation styles, setup CTA, menu

--- src/components/Navigation/Navigation.ts
+++ src/components/Navigation/Navigation.ts
@@ -1,20 +1,30 @@
 import { html, css, LitElement } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { customElement, property } from 'lit/decorators.js';
 import './Navigation.css';
 
 const styles = css`
   :host {
     position: fixed;
-    display: flex;
-    justify-content: center;
-    align-items: center;
     left: 0;
     top: 0;
     right: 0;
     z-index: 80;
-    height: 5rem;
     background: white;
     box-shadow: 0 0.25rem 1rem #DDD, 0 0.5rem 1rem #EEE, 0 0.75rem 1rem #FFF;
+  }
+
+  nav {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    height: 4rem;
+    padding: 1rem 2rem;
+  }
+
+  img.logo {
+    position: relative;
+    height: 100%;
   }
 `;
 
@@ -22,10 +32,15 @@ const styles = css`
 export default class Navigation extends LitElement {
   static styles = styles;
 
+  @property({ type: String })
+  public logoSrc = '';
+
   protected render() {
     return html`
       <nav>
+        <img class="logo" src="${this.logoSrc}" />
         <slot name="main-menu"></slot>
+        <slot name="main-cta"></slot>
       </nav>
     `;
   }
