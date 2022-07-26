a5865d2cc184a677604cfcb43cc532754103e771
tlhfckoctbcr <stu@campgrantnc.com>
2022-07-26 14:02:06 -0400

    Add postcss-nested

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,6 @@
 import Button from './components/Button/Button';
 import Navigation from './components/Navigation/Navigation';
+import './styles/main.css';
 
 declare const components: {
     Button: typeof Button;
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,22 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { dirname, resolve } from 'path';
 import { defineConfig } from 'vite';
+import autoprefixer from 'autoprefixer';
+import postcssNested from 'postcss-nested';
 
 function generateFileName(format) {
   return `kps.${format}.js`;
 }
 
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer,
+        postcssNested,
+      ],
+    },
+  },
   build: {
     lib: {
       name: 'kps-wc',
