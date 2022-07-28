91d40dd0f5a53698ca6abd026f6f61d9284c851f
tlhfckoctbcr <stu@campgrantnc.com>
2022-07-28 16:05:50 -0400

    All web utilities kept in the web-components lib

--- src/modules/HeroBanner/HeroBanner.ts
+++ src/modules/HeroBanner/HeroBanner.ts
@@ -5,16 +5,38 @@ import { customElement, property } from 'lit/decorators.js';
 class HeroBanner extends LitElement {
   static styles = css`
     .wrap {
+      display: flex;
+      align-items: center;
       width: 100%;
       height: auto;
       min-height: 40rem;
-      background: var(--color-primary);
+      background: var(--color-black);
+    }
+
+    .kps-hero__content {
+      width: 100%;
+      max-width: var(--page-max-width);
+      padding: 0 2rem;
+      margin: 0 auto;
     }
 
     h1 {
       font-size: var(--font-size-7xl);
       font-weight: var(--font-weight-light);
+      line-height: 1;
+      color: var(--color-secondary-lighter);
       margin: 0;
+    }
+
+    h2 {
+      font-size: var(--font-size-2xl);
+      font-weight: var(--font-weight-light);
+      color: var(--color-secondary-lighter);
+      margin: 0;
+    }
+
+    p {
+      color: var(--color-white);
     }
   `;
 
