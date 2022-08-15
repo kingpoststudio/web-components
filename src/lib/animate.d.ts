343c39ab1f38ad9ca929b2d728d21706568a71d8
tlhfckoctbcr <stu@campgrantnc.com>
2022-08-15 16:33:23 -0400

    Remove animate until ready

--- src/lib/animate.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const easeOutCubic: (t: number) => number;
-export declare const animate: (duration: number, callback: (t: number) => void, options?: {
-    easing?: ((t: number) => number) | undefined;
-    range?: [number, number] | undefined;
-}) => void;
