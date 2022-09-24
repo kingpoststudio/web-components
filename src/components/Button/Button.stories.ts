a28e73426ea165c6cb74e221836be2f9db74d4cd
Stuart Grant <stu@campgrantnc.com>
2022-09-23 21:16:30 -0400

    Button stories, for color options + icons

--- src/components/Button/Button.stories.ts
+++ src/components/Button/Button.stories.ts
@@ -8,6 +8,15 @@ export default {
 };
 
 export const Colors = () => html`
+  <kps-button color="primary">Primary</kps-button>
+  <kps-button color="secondary">Secondary</kps-button>
+  <kps-button color="success">Success</kps-button>
+  <kps-button color="warning">Warning</kps-button>
+  <kps-button color="error">Danger</kps-button>
+  <kps-button color="transparent">Info</kps-button>
+`;
+
+export const Icons = () => html`
   <kps-button-group direction="row">
     <kps-button size="icon" variant="outline"><kps-icon icon="linkedin"></kps-icon></kps-button>
     <kps-button size="icon" variant="outline"><kps-icon icon="twitter"></kps-icon></kps-button>
--- src/components/Container/Container.stories.ts
+++ src/components/Container/Container.stories.ts
@@ -8,14 +8,12 @@ export default {
   component: 'kps-container',
 };
 
-export const Container = () => html`
+const InnerContent = html`
+  <div style="display:flex;width:full;height:16rem;border:1px solid red;"></div>
+`;
+
+export const Default = () => html`
   <kps-container>
-    <div style="width:fit-content;position:relative;">
-      <img src="https://21683844.fs1.hubspotusercontent-na1.net/hubfs/21683844/raw_assets/public/NoahMedicalGalaxyTheme/assets/images/galaxy.png">
-      <div style="width:2rem;height:2rem;background:red;top:10%;left:22%;position:absolute;"></div>
-      <div style="width:2rem;height:2rem;background:blue;top:49%;left:38%;position:absolute;"></div>
-      <div style="width:2rem;height:2rem;background:green;top:22%;left:60%;position:absolute;"></div>
-      <div style="width:2rem;height:2rem;background:yellow;top:65%;left:70%;position:absolute;"></div>
-    </div>
+    ${InnerContent}
   </kps-container>
 `;
