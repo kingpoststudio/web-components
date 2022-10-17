af4b99898327b110c130c6bf1a854792588328f2
Stuart Grant <stu@campgrantnc.com>
2022-10-17 06:24:36 -0400

    Progress

--- src/modules/Pagination/Pagination.ts
+++ src/modules/Pagination/Pagination.ts
@@ -6,13 +6,53 @@ import Styles from './Pagination.css';
 export default class Pagination extends LitElement {
   static styles = [unsafeCSS(Styles)];
 
+  constructor() {
+    super();
+    this.generateButton = this.generateButton.bind(this);
+  }
+
+  @property({ type: Number })
+  private currentPage = 1;
+
+  @property({ type: Number })
+  private pageCount = 100;
+
+  @property({ type: String })
+  private urlParam = 'page';
+
+  goToPage(page: number) {
+    if (page === this.currentPage) return;
+
+    const url = new URL(window.location.href);
+    const params = new URLSearchParams(url.search);
+
+    params.set(this.urlParam, page.toString());
+    window.history.pushState({}, '', `${url.pathname}?${params.toString()}`);
+    this.currentPage = page;
+  }
+
+  generateButton(i: number) {
+    const loopPage = this.currentPage + i;
+    const isCurrentPage = loopPage === this.currentPage;
+    const isInRange = loopPage > 0 && loopPage <= this.pageCount;
+
+    return isInRange ? html`
+      <a
+        class="block ${isCurrentPage ? 'active' : ''}"
+        @click=${() => this.goToPage(loopPage)}
+        aria-label="Go to page ${this.currentPage + i}"
+        role="button"
+      >${this.currentPage + i}</a>`
+      : null;
+  }
+
   render() {
     return html`
       <div id="pagination">
         <nav aria-label="Pagination" role="navigation">
           <a
             class="block"
-            @click=${() => { }}
+            @click=${() => this.goToPage(1)}
             rel="prev"
             role="button"
             aria-label="Go to first page"
@@ -24,7 +64,7 @@ export default class Pagination extends LitElement {
           </a>
           <a
             class="block"
-            @click=${() => { }}
+            @click=${() => this.goToPage(this.currentPage - 1)}
             rel="prev"
             role="button"
             aria-label="Go to previous page"
@@ -33,20 +73,12 @@ export default class Pagination extends LitElement {
               <polygon points="0 0 6.5 8 13 0 10.87 0 6.5 5.43 2.06 0 0 0"/>
             </svg>
           </a>
-          ${Array.from({ length: 6 }, (_, i) => i - 2).map((i) => html`
-            <a
-              class="block"
-              @click=${() => { }}
-              aria-label="Go to page ${i}"
-              aria-current=${i === 0 ? 'page' : 'false'}
-              role="button"
-            >
-              ${i}
-            </a>
-          `)}
+
+          ${Array.from({ length: 6 }, (_, i) => i - 2).map(this.generateButton)}
+
           <a
               class="block"
-              @click=${() => { }}
+              @click=${() => this.goToPage(this.currentPage + 1)}
               rel="next"
               role="button"
               aria-label="Go to next page"
@@ -57,7 +89,7 @@ export default class Pagination extends LitElement {
           </a>
           <a
               class="block"
-              @click=${() => { }}
+              @click=${() => this.goToPage(this.pageCount)}
               aria-label="Go to last page"
               role="button"
               rel="last"
