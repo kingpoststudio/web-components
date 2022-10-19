e940139f3f738db03d4d75fb613fe2c40f726312
Stuart Grant <stu@campgrantnc.com>
2022-10-18 21:29:57 -0400

    Style and setup repeater for topic options

--- src/modules/TopicFilter/TopicFilter.ts
+++ src/modules/TopicFilter/TopicFilter.ts
@@ -21,7 +21,44 @@ export default class TopicFilter extends LitElement {
     title = 'Topic Filters';
 
   @property({ type: Array })
-  private topics: Array<Topic> = [];
+  private topics: Array<Topic> = [
+      {
+        name: 'Topic 1',
+        id: 'topic-1',
+        options: [
+          {
+            id: 'topic-1-option-1',
+            name: 'Topic 1 Option 1',
+          },
+          {
+            id: 'topic-1-option-2',
+            name: 'Topic 1 Option 2',
+          },
+          {
+            id: 'topic-1-option-3',
+            name: 'Topic 1 Option 3',
+          },
+        ],
+      },
+      {
+        name: 'Topic 2',
+        id: 'topic-2',
+        options: [
+          {
+            id: 'topic-2-option-1',
+            name: 'Topic 2 Option 1',
+          },
+          {
+            id: 'topic-2-option-2',
+            name: 'Topic 2 Option 2',
+          },
+          {
+            id: 'topic-2-option-3',
+            name: 'Topic 2 Option 3',
+          },
+        ],
+      },
+    ];
 
   constructor() {
     super();
@@ -40,8 +77,23 @@ export default class TopicFilter extends LitElement {
         </div>
 
         <div class="topics">
-          
+          ${this.topics.map((topic) => html`
+            <div class="topic">
+              <div class="header">
+                <h4>${topic.name}</h4>
+              </div>
+              <div class="options">
+                ${topic.options.map((option) => html`
+                  <div class="option">
+                    <input type="checkbox" id="${option.id}" name="${option.name}" value="${option.id}">
+                    <label for="${option.id}">${option.name}</label>
+                  </div>
+                  `)}
+              </div>
+            </div>
+          `)}
         </div>
+
       </div>
     `;
   }
