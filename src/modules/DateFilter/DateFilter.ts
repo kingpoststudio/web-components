7277592727da8cfc902874910a60adc2963cd73a
Stuart Grant <stu@campgrantnc.com>
2022-11-22 06:40:05 -0500

    On dateFormChange handler

--- src/modules/DateFilter/DateFilter.ts
+++ src/modules/DateFilter/DateFilter.ts
@@ -21,14 +21,13 @@ const months = [
 export default class DateFilter extends LitElement {
   static styles = [unsafeCSS(Styles)];
 
-  @property({ type: Boolean })
-  isFilteringActive = false;
+  private dateValue = '';
 
   @property({ type: String })
-  title = 'Filter by Date';
+    title = 'Filter by Date';
 
   @property({ type: String })
-  urlParam = 'selected_date';
+    urlParam = 'selected_date';
 
   firstUpdated() {
     this.setActiveDateValue();
@@ -37,14 +36,33 @@ export default class DateFilter extends LitElement {
   setActiveDateValue() {
     const url = new URL(window.location.href);
     const params = new URLSearchParams(url.search);
+
+    this.dateValue = params.get(this.urlParam) || '';
   }
 
   clearDateFilter() {
     const url = new URL(window.location.href);
     const params = new URLSearchParams(url.search);
+    params.delete(this.urlParam);
     params.delete('page');
 
     window.location.href = `${url.pathname}?${params.toString()}`;
+  }
+
+  onFormSelectChange(e: Event) {
+    const form = (e.target as HTMLElement).closest('form') as HTMLFormElement;
+    const formData = new FormData(form);
+    const month = formData.get('month') as string;
+    const year = formData.get('year') as string;
+
+    if (month && year) {
+      const url = new URL(window.location.href);
+      const params = new URLSearchParams(url.search);
+      params.set(this.urlParam, `${month}-${year}`);
+      params.delete('page');
+
+      window.location.href = `${url.pathname}?${params.toString()}`;
+    }
   }
 
   render() {
@@ -52,19 +70,19 @@ export default class DateFilter extends LitElement {
       <div class="wrap">
         <div class="intro">
           <p>${this.title}</p>
-          ${this.isFilteringActive ? html`<a class="clear" @click=${this.clearDateFilter}>Clear</a>` : ''}
+          ${this.dateValue ? html`<a class="clear" @click=${this.clearDateFilter}>Clear</a>` : ''}
         </div>
 
-        <div class="date-selector">
-          <select>
+        <form class="date-selector" @change=${this.onFormSelectChange}>
+          <select name="month">
             <option value="" disabled selected>Month</option>
             ${months.map((month, i) => html`<option value="${i + 1}">${month}</option>`)}
           </select>
-          <select>
+          <select name="year">
             <option value="" disabled selected>Year</option>
             ${Array.from(Array(10).keys()).map((year) => html`<option value="${year + 2021}">${year + 2021}</option>`)}
           </select>
-        </div>
+        </form>
 
       </div>
     `;
