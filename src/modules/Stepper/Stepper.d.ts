ddca532269f2d1374091dd8397772ca89174c3ef
Stuart Grant <stu@campgrantnc.com>
2022-12-18 09:56:32 -0500

    Run build

--- src/modules/Stepper/Stepper.d.ts
+++ src/modules/Stepper/Stepper.d.ts
@@ -8,6 +8,10 @@ interface StepBlock {
     position: {
         x: number;
         y: number;
+        mobile?: {
+            x: number;
+            y: number;
+        };
     };
     slides: {
         from: number;
@@ -22,9 +26,11 @@ export default class Stepper extends LitElement {
     visible: boolean;
     speed: 'slow' | 'normal' | 'fast';
     fade: boolean;
+    isMobile: boolean;
     animationRef: import("lit-html/directives/ref").Ref<Element>;
     constructor();
     disconnectedCallback(): void;
+    handleResize: () => void;
     handleScroll: () => void;
     getBlockContent(block: StepBlock): import("lit").TemplateResult<1>;
     getImageContent(image: StepImage): import("lit").TemplateResult<1>;
